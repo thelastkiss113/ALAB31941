772900019d05ba887303b93c9d5e26e2bcdbabdf
Jessica <jessica.blum.model@gmail.com>
2024-11-02 13:02:30 -0700

    first commit

---
+++ db/conn.mjs
@@ -0,0 +1,15 @@
+//db/conn.mjs
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.ATLAS_URI);
+
+let conn;
+try {
+  conn = await client.connect();
+} catch (e) {
+  console.error(e);
+}
+
+let db = conn.db("sample_training");
+
+export default db;
